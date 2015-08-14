fc68d5f58ca14c8ad6f63503ed0ca679854936d8
Jordan Johansen <jordanjohan@gmail.com>
2015-08-14 12:29:28 -0700

    Change countRepeats method to only return full matches.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,7 +3,18 @@
     {
         function countRepeats($string, $keyword)
         {
-            return substr_count(strtolower($string), strtolower($keyword));
+            $count = 0;
+            $string_array = explode(" ", $string);
+            foreach($string_array as $search_string) {
+                if (preg_match(strtolower("/\b$keyword\b/"), strtolower($search_string))) {
+                    $count += 1;
+                }
+            }
+
+            return $count;
+
+            /* I was really stoked about solving this entire thing with one line, but unfortunately I forgot to make sure it didn't return partial matches until the I reread the prompt at the very end.  It could have been so beautiful.
+            return substr_count(strtolower($string), strtolower($keyword)); */
         }
     }
 ?>
