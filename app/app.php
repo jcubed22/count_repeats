f23a70db46183fd78762a153b39be862d248a10b
Jordan Johansen <jordanjohan@gmail.com>
2015-08-14 11:01:16 -0700

    Fix typos.

--- app/app.php
+++ app/app.php
@@ -1,11 +1,11 @@
 <?php
-    require_once __DIR__."/../vendor/autolaod.php";
+    require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
 
     $app = new Silex\Application();
     $app['debug'] = true;
 
-    $app->register(new Silex\Provider\TwigServiceProvicer(), array('twig.path' => __DIR__.'/../views'));
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use ($app) {
         return $app['twig']->render('search_form.html.twig');
@@ -13,7 +13,7 @@
 
     $app->get('/search_form', function() use ($app) {
         $my_RepeatCounter = new RepeatCounter;
-        $repeat_count = $my_RepeatCounter->countRepeats($_GET['string'], $_GET['keyword']);
+        $repeat_count = $my_RepeatCounter->countRepeats($_GET['search_string'], $_GET['keyword']);
         return $app['twig']->render('results.html.twig', array('repeats' => $repeat_count));
     });
 
